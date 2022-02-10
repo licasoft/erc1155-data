6e0fc23f4b5583d8099ce6b527e1c6cfcf397c0f
licasoft <99399850+licasoft@users.noreply.github.com>
2022-02-10 15:27:44 +0100

    Update erc-1155-licasoft.js

--- erc-1155-licasoft.js
+++ erc-1155-licasoft.js
@@ -3,7 +3,7 @@ pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract MyCollectibles is ERC1155 {
+contract licasoft is ERC1155 {
     constructor() ERC1155("https://github.com/licasoft/erc1155-data/{id}.json") {
         _mint(msg.sender, 1 /* Id licasoft 1 */, 1000 /* Cantidad */, "");
         _mint(msg.sender, 2 /* Id licasoft 2 */, 1000 /* Cantidad */, "");
