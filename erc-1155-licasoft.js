00e5fd4071736f9cb2317016816d8a9a6b387596
licasoft <99399850+licasoft@users.noreply.github.com>
2022-02-10 13:04:05 +0100

    Create erc-1155-licasoft.js

---
+++ erc-1155-licasoft.js
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MyCollectibles is ERC1155 {
+    constructor() ERC1155("https://github.com/licasoft/erc1155-data/{id}.json") {
+        _mint(msg.sender, 1 /* Id licasoft 1 */, 1000 /* Cantidad */, "");
+        _mint(msg.sender, 2 /* Id licasoft 2 */, 1000 /* Cantidad */, "");
+    }
+}
