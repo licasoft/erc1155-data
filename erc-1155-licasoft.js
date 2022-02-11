7b1541bd552beca603118254dfe08c44ee186197
licasoft <99399850+licasoft@users.noreply.github.com>
2022-02-11 09:49:45 +0100

    Update erc-1155-licasoft.js

--- erc-1155-licasoft.js
+++ erc-1155-licasoft.js
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: MIT
 
-// deploy https://remix.ethereum.org/
-
-
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
+
+
 contract licasoft is ERC1155 {
-    constructor() ERC1155("https://github.com/licasoft/erc1155-data/{id}.json") {
-        _mint(msg.sender, 1 /* Id licasoft 1 */, 1000 /* Cantidad */, "");
-        _mint(msg.sender, 2 /* Id licasoft 2 */, 1000 /* Cantidad */, "");
+    
+    uint256 public constant Licasoft1 = 1;
+    uint256 public constant Licasoft2 = 2;
+  
+// nombre de la Coleccion
+    string public name = "My Collection Licasoft";
+    string public symbol = "LICA";
+
+    constructor() ERC1155("https://utilizame.com/opensea/data/{id}.json") {
+        _mint(msg.sender, Licasoft1 , 2000 , "lica1");
+        _mint(msg.sender, Licasoft2 , 2000 , "lica2");
+
     }
 }
